265e5f187226651ddedd54361df3235716513543
Guantong <i@gt.email>
2024-05-30 14:38:12 +0800

    check ponder&api

--- server/plugins/cron.js
+++ server/plugins/cron.js
@@ -22,6 +22,8 @@ export default fp(async (fastify) => {
         allWarns.push(await checkOracleSignerBalance());
         // allWarns.push(await checkOracleSignerSubmition());
         allWarns.push(await checkTimeoutRelay());
+        allWarns.push(await checkMsgportAPI());
+        allWarns.push(await checkPonder());
         fastify.log.warn(allWarns);
         await notify(allWarns, "59154,77764,55181,63157,49833");
     }, {
@@ -196,6 +198,38 @@ export default fp(async (fastify) => {
         } catch (e) {
             fastify.log.error(e);
             warns.push(e);
+        }
+        return warns;
+    }
+
+    async function checkMsgportAPI() {
+        const warns = [];
+        try {
+            const result = await axios.get("http://api.msgport.xyz/ormp/fee?from_chain_id=728126428&to_chain_id=46&from_address=0x9F33a4809aA708d7a399fedBa514e0A0d15EfA85&to_address=0x9F33a4809aA708d7a399fedBa514e0A0d15EfA85&payload=0x1841a477000000000000000000000000000000000000000000000000000000000000002e0000000000000000000000002b496f19a420c02490db859fefeccd71edc2c046000000000000000000000000dc0c760c0fb4672d06088515f6446a71df0c64c1000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000001a4e80f03d0000000000000000000000000000000000000000000000000000000000000002e000000000000000000000000e7578598aac020abfb918f33a20fad5b71d670b4000000000000000000000000092e19c46c9daab7824393f1cd9c22f5bea135600000000000000000000000004ca75992d2750bec270731a72dfdede6b9e71cc700000000000000000000000088a39b052d477cfde47600a7c9950a441ce61cb40000000000000000000000000000000000000000000000056bc75e2d631000000000000000000000000000000000000000000000000000000000018e98a317b100000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000080000000000000000000000000c29dcb1f12a1618262ef9fba673b77140adc02d60000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000001488a39B052d477CfdE47600a7C9950a441Ce61cb400000000000000000000000000000000000000000000000000000000000000000000000000000000&refund_address=0x9F33a4809aA708d7a399fedBa514e0A0d15EfA85", { timeout: 10000 });
+            const data = result.data;
+            console.log(data);
+            if (data.code != 0 || !data.data.fee) {
+                warns.push("Msgport API error" + ` code: ${data.code}, time: ${Date.now()}`);
+            }
+        } catch (e) {
+            fastify.log.error(e);
+            warns.push("Msgport API error" + e + ", " + Date.now());
+        }
+        return warns;
+    }
+
+    async function checkPonder() {
+        const warns = [];
+        try {
+            const result = await axios.get("https://ormponder.msgport.xyz/", { timeout: 3000 });
+            const data = result.data;
+            console.log(data);
+            if (!data.includes("Ponder Playground")) {
+                warns.push("Msgport Ponder error" + ` time: ${Date.now()}`);
+            }
+        } catch (e) {
+            fastify.log.error(e);
+            warns.push("Msgport Ponder error" + e + ", " + Date.now());
         }
         return warns;
     }

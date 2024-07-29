d2e1a2c1c3d5fe9fefc810c577adff15c3c2e77f
Guantong <i@gt.email>
2024-07-29 10:55:17 +0800

    update timeout 1hour

--- server/plugins/cron.js
+++ server/plugins/cron.js
@@ -196,7 +196,7 @@ export default fp(async (fastify) => {
         const oneDayAgo = now - 24 * 60 * 60;
         // const oneDayAgo = 1719701000;
         const halfHourAgo = now - 30 * 60;
-        const towHoursAgo = now - 60 * 60 * 2;
+        const oneHourAgo = now - 60 * 60 * 1;
         const query = `query MessageList($timestampGt: numeric!, $timestampLt: numeric!) {
   MessagePort(order_by: {sourceBlockTimestamp: desc},
   	where: {
@@ -255,7 +255,7 @@ export default fp(async (fastify) => {
                 query: query,
                 variables: {
                     timestampGt: oneDayAgo,
-                    timestampLt: towHoursAgo
+                    timestampLt: oneHourAgo
                 }
             });
             const data = result.data.data.MessagePort;
